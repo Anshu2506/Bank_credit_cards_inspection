1654c08b4efd406da585b404fbdea23201a6dd0c
Anshu2506 <aky8109627220@gmail.com>
2026-04-12 22:13:33 -0700

    pickle file added

--- src/mlproject/components/data_ingestion.py
+++ src/mlproject/components/data_ingestion.py
@@ -20,7 +20,7 @@ class DataIngestion:
 
     def initiate_data_ingestion(self):
         try:
-            df=read_sql_data()
+            df=pd.read_csv(os.path.join('notebook/data','raw.csv'))
             logging.info("reading data from sql is completed")
             os.makedirs(os.path.dirname(self.ingestion_config.raw_data_path),exist_ok=True)
 
